fe3d73ac6634ba965b616cb6429e6eaf3563b223
sueyin <sueyin@users.noreply.github.com>
2017-12-18 21:50:19 +1300

    direction implemented

--- Board.java
+++ Board.java
@@ -1,12 +1,20 @@
 package sample;
 
+
+import javafx.application.Platform;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Board {
 
-    private final int BOARD_WIDTH = 50;
-    private final int BOARD_HEIGHT = 50;
+    private final int BOARD_WIDTH = 500;
+    private final int BOARD_HEIGHT = 500;
     private final int START_LENGTH = 3;
-    private final int[] DEFAULT_START_POINT = {20,20}; //default snake facing left;
+    private final int[] DEFAULT_START_POINT = {200,200}; //default snake facing left;
+    private final Direction DEFAULT_START_DIRECTION = Direction.LEFT;
     private final int DEFAULT_FRUIT_NUM = 5;
+    private final int START_SPEED = 500;
 
 
     private MainPageController _controller;
@@ -20,23 +28,34 @@ public class Board {
     private int[] x_fruit = new int[DEFAULT_FRUIT_NUM];
     private int[] y_fruit = new int[DEFAULT_FRUIT_NUM];
 
+    private Timer _timer;
+    private int _speed;  //snake movement in millisecond;
+
+    private Direction _direction;
+
     public Board(MainPageController controller) {
         _controller = controller;
         _snakeLength = START_LENGTH;
         _fruitNum = 0;
+        _speed = START_SPEED;
+        _direction = DEFAULT_START_DIRECTION;
 
         initialiseSnake();
         initialiseFruit();
+
+        setUpTimer();
     }
 
 
     private void initialiseSnake() {
-        for(int i = 0; i < _snakeLength; i++) {
-            x_snake[i] = DEFAULT_START_POINT[0]+1;
-            y_snake[i] = DEFAULT_START_POINT[1];
+        for(int i = _snakeLength; i>0; i--) {
+            System.out.println(""+_direction.getX());
+            x_snake[i-1] = DEFAULT_START_POINT[0]-_direction.getX()*i;
+            y_snake[i-1] = DEFAULT_START_POINT[1] -_direction.getY()*i;
         }
 
         paintSnake();
+        System.out.println("initialise snake");
     }
 
     private void initialiseFruit() {
@@ -50,23 +69,86 @@ public class Board {
         for (int i = 0; i < _snakeLength; i++) {
             if(i == 0) {
                 _controller.paintHead(x_snake[0],y_snake[0]);
+                System.out.println("Head "+ x_snake[0]+ y_snake[0]);
             }
             else if (i == _snakeLength -1) {
                 _controller.paintTail(x_snake[i],y_snake[i]);
+                System.out.println("Tail "+ x_snake[i]+y_snake[i]);
             }
             else {
                 _controller.paintBody(x_snake[i],y_snake[i]);
+                System.out.println("Body "+ x_snake[i]+y_snake[i]);
             }
         }
     }
 
     private void plantFruit() {
-        int x_pos = (int)Math.random()*BOARD_WIDTH;
-        int y_pos = (int)Math.random()*BOARD_HEIGHT;
+        int x_pos = (int)(Math.random() * BOARD_WIDTH);
+        int y_pos = (int)(Math.random() * BOARD_HEIGHT);
 
-        x_fruit[_fruitNum-1] = x_pos;
-        y_fruit[_fruitNum-1] = y_pos;
+        x_fruit[_fruitNum] = x_pos;
+        y_fruit[_fruitNum] = y_pos;
 
         _controller.paintFruit(x_pos, y_pos);
+        System.out.println("plant fruit");
+    }
+
+    private void moveSnake() {
+
+
+        for(int i = _snakeLength; i > 0; i--) {
+            x_snake[i] = x_snake[i-1];
+            y_snake[i] = y_snake[i-1];
+        }
+        x_snake[0] = x_snake[0]+_direction.getX();
+        y_snake[0] = y_snake[0]+_direction.getY();
+
+        _controller.clear();
+        paintSnake();
+    }
+
+    private void setUpTimer() {
+        _timer = new Timer();
+
+        TimerTask updateSnake = new TimerTask() {
+            public void run() {
+                Platform.runLater(() -> {
+                    moveSnake();
+                    System.out.println("timer");
+                });
+            }
+        };
+
+        TimerTask increaseSpeed = new TimerTask() {
+            public void run() {
+                _speed = _speed - 500;
+            }
+        };
+
+        _timer.scheduleAtFixedRate(updateSnake, 0,_speed);
+        _timer.scheduleAtFixedRate(increaseSpeed,0,7001);
+    }
+    public void setDirection(Direction direction) {
+        _direction = direction;
+    }
+}
+
+enum Direction {
+    LEFT(-10,0),RIGHT(10,0),UP(0,-10),DOWN(0,10);
+
+    private int _x;
+    private int _y;
+
+    Direction(int x, int y) {
+       _x = x;
+       _y = y;
+    }
+
+    public int getX() {
+        return _x;
+    }
+
+    public int getY() {
+        return _y;
     }
 }
--- MainPageController.java
+++ MainPageController.java
@@ -1,20 +1,111 @@
 package sample;
 
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+import java.util.ArrayList;
+
 public class MainPageController {
 
+    private ArrayList<Circle> _previousTail = new ArrayList<Circle>();
+
+    @FXML
+    private Button _button;
+
+    @FXML
+    private AnchorPane _pane;
+
+    private Board _board;
+
+    @FXML
+    public void initialize() {
+        _board = new Board(this);
+
+        _button.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+            @Override
+            public void handle(KeyEvent event) {
+                if(event.getCode() == KeyCode.UP) {
+                    _board.setDirection(Direction.UP);
+                }
+                else if (event.getCode() == KeyCode.DOWN) {
+                    _board.setDirection(Direction.DOWN);
+                }
+                else if(event.getCode() == KeyCode.RIGHT) {
+                    _board.setDirection(Direction.RIGHT);
+                }
+                else if (event.getCode() == KeyCode.LEFT) {
+                    _board.setDirection(Direction.LEFT);
+                }
+            }
+        });
+
+
+
+    }
+
+    @FXML
+    public void handleKeyPressed(KeyEvent event) {
+        if(event.getCode() == KeyCode.UP) {
+            _board.setDirection(Direction.UP);
+            System.out.println("UP");
+        }
+        else if (event.getCode() == KeyCode.DOWN) {
+            _board.setDirection(Direction.DOWN);
+        }
+        else if(event.getCode() == KeyCode.RIGHT) {
+           _board.setDirection(Direction.RIGHT);
+        }
+        else if (event.getCode() == KeyCode.LEFT) {
+            _board.setDirection(Direction.LEFT);
+        }
+        event.consume();
+    }
+
+    @FXML
     public void paintHead(int x, int y) {
-
+        Circle circle = new Circle(x, y,3);
+        circle.setFill(Color.PINK);
+        _pane.getChildren().add(circle);
+        _previousTail.add(circle);
     }
 
+    @FXML
     public void paintBody(int x, int y) {
-
+        Circle circle = new Circle(x, y,3);
+        circle.setFill(Color.GREEN);
+        _pane.getChildren().add(circle);
+        _previousTail.add(circle);
     }
 
+    @FXML
     public void paintTail(int x, int y) {
-
+        Circle circle = new Circle(x, y,3);
+        circle.setFill(Color.PURPLE);
+        _pane.getChildren().add(circle);
+        _previousTail.add(circle);
     }
 
+    @FXML
     public void paintFruit(int x, int y) {
-
+        Circle circle = new Circle(x, y,3);
+        circle.setFill(Color.RED);
+        _pane.getChildren().add(circle);
     }
+
+    @FXML
+    public void clear() {
+        for(Circle c: _previousTail){
+            _pane.getChildren().remove(c);
+        }
+    }
+
 }
